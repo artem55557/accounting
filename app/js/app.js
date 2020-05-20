356ddbe418e37127df1fb9c652169c5130803418
Artem Netesov <artem@ASUS-PC.localdomain>
2020-05-20 12:40:29 +0700

    Сверстана шапка сайта

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
+  // Custom JS
+  const burger = document.querySelector('.burger-btn')
+  const menu = document.querySelector('.menu')
+  burger.addEventListener('click', event => {
+    burger.classList.toggle('open')
+    menu.classList.toggle('menu-open')
+  })
 
 });
