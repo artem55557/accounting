addd8dd75ef16939aaf0d11fcdde7e41482f64f5
Artem Netesov <artem@ASUS-PC.localdomain>
2020-05-21 10:31:51 +0700

    Сверстана страница логина

--- app/js/app.js
+++ app/js/app.js
@@ -3,10 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
   // Custom JS
   const burger = document.querySelector('.burger-btn')
   const menu = document.querySelector('.menu')
-  burger.addEventListener('click', event => {
-    event.preventDefault()
-    burger.classList.toggle('open')
-    menu.classList.toggle('menu-open')
-  })
+
+  if(burger) {
+    burger.addEventListener('click', event => {
+      event.preventDefault()
+      burger.classList.toggle('open')
+      menu.classList.toggle('menu-open')
+    })
+  }
 
 });
